0fc86e83574aa428198dad76ef1f3ad45a52dd43
wylzyx <wanyu990124@163.com>
2019-11-22 11:53:57 +0800

    19-11-22

---
+++ Btree/Queue.h
@@ -0,0 +1,32 @@
+#pragma  once
+#include "BinaryTree.h"
+//单链表实现队列
+typedef BTNode* DataType;
+
+typedef struct QueueNode
+{
+	DataType _data;
+	struct QueueNode* _next;
+}QueueNode;
+
+typedef struct Queue
+{
+	QueueNode* _front;
+	QueueNode* _rear;
+}Queue;
+
+void QueueInit(Queue* q);
+void QueueDestroy(Queue* q);
+//队尾插入元素
+void QueuePush(Queue* q, DataType x);
+//队头删除元素
+void QueuePop(Queue* q);
+//获取队头元素
+DataType QueueFront(Queue* q);
+//获取队尾元素
+DataType QueueBack(Queue* q);
+
+int QueueEmpty(Queue* q);
+int QueueSize(Queue* q);
+
+void QueuePrint(Queue* q);
